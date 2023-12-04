f513e44994d2e68019e9c2360aedbd5779d0b733
Fabiano Lages <fabiano.lages@tenti.com.br>
2023-12-04 02:45:55 -0300

    Telas de cliente e orçamento criados, falta a lógica do formulário

--- src/routes/cliente.js
+++ src/routes/cliente.js
@@ -1,9 +1,89 @@
 //const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 const routerCliente = require("express").Router();
+require("../models/Cliente");
+
+const Cliente = mongoose.model("clientes");
+
+routerCliente.get("/", async (req, res) => {
+    const clientes = [];
+    const rst = await Cliente.find().populate("bairro", "cidade", "estado").sort({nome: "asc"});
+    if(rst) {
+        rst.forEach(cli => {
+            clientes.push({
+                bairro: {
+                    id: cli._id,
+                    nome: cli.nome,
+                    numero: cli.numero,
+                    complemento: cli.complemento,
+                    bairro: cli.bairro,
+                    cidade: cli.cidade,
+                    estado: cli.estado,
+                    cep: cli.cep,
+                    created: `${cli.created.toLocaleDateString("pt-BR")} ${cli.created.toLocaleTimeString("pt-BR")}`,
+                    modified: `${cli.modified.toLocaleDateString("pt-BR")} ${cli.modified.toLocaleTimeString("pt-BR")}`
+                }
+            });
+        });
+
+        res.render("cliente/", {clientes});
+    }
+});
 
 
-routerCliente.get("/", (req, res) => {
-    res.render("cliente/");
+routerCliente.post("/add", (req, res) => {
+    const destino = "/Cliente";
+    const nomeObjeto = "Cliente";
+
+    if(req.body.idCliente) {
+        Cliente.findOne({_id: req.body.idBairro})
+            .then((cliente) => {
+                cliente.nome = req.body.txtNomeCliente;
+                cliente.modified = new Date();
+
+                cliente.save()
+                    .then(() => {
+                        req.flash("Log", `${nomeObjeto} ${req.body.txtNomeCliente} editado com sucesso`);
+                        res.redirect(destino);
+                    })
+                    .catch((err) => {
+                        req.flash("Erro", `Erro ao editar o ${nomeObjeto.toLowerCase()} ${req.body.txtNomeCliente}. ${err}`);
+                        res.redirect(destino);
+                    });
+            })
+            .catch((err) => {
+                req.flash("Erro", `Erro ao editar o ${nomeObjeto.toLowerCase()} ${req.body.txtNomeCliente}. ${err}`);
+                res.redirect(destino);
+            });
+    } else {
+        new Cliente(
+            {
+                nome: req.body.txtNomeClioente,
+                cidade: new mongoose.Types.ObjectId(req.body.hdCidade)
+            })
+            .save()
+            .then(() => {
+                req.flash("Log", `${nomeObjeto} ${req.body.txtNomeCliente} incluído com sucesso`);
+                res.redirect(destino);
+            })
+            .catch((err) => {
+                req.flash("Erro", `Erro ao incluir o ${nomeObjeto.toLowerCase()} ${req.body.txtNomeCliente}. ${err}`);
+                res.redirect(destino);
+            });
+    }
+});
+
+routerCliente.post("/del", async (req, res) => { 
+    const destino = "/Cliente";
+    Cliente.deleteOne({_id: req.body.idCliente})
+        .then(() => {
+            req.flash("Log", `Cliente ${req.body.txtNomeCliente} removido`);
+            res.redirect(destino);
+        })
+        .catch((err) => {
+            req.flash("Erro", `Erro ao apagar o cliente ${req.body.txtNomeCliente}. ${err}`);
+            res.redirect(destino);
+        });
 });
 
 module.exports = { routerCliente };
--- src/routes/orcamento.js
+++ src/routes/orcamento.js
@@ -1,8 +1,88 @@
-//const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 const routerOrca = require("express").Router();
+require("../models/Orcamento");
 
-routerOrca.get("/", (req, res) => {
-    res.render("orcamento/");
+const Orcamento = mongoose.model("orcamentos");
+
+routerOrca.get("/", async (req, res) => {
+    const orcamentos = [];
+    const rst = await Orcamento.find().populate("cliente").sort({cliente: "asc"});
+    if(rst) {
+        rst.forEach(orc => {
+            orcamentos.push({
+                bairro: {
+                    id: orc._id,
+                    cliente: orc.cliente,
+                    descricao: orc.descricao,
+                    complemento: orc.complemento,
+                    valor: orc.valor,
+                    created: `${orc.created.toLocaleDateString("pt-BR")} ${orc.created.toLocaleTimeString("pt-BR")}`,
+                    modified: `${orc.modified.toLocaleDateString("pt-BR")} ${orc.modified.toLocaleTimeString("pt-BR")}`
+                }
+            });
+        });
+
+        res.render("orcamento/", {orcamentos});
+    }
+});
+
+
+routerOrca.post("/add", (req, res) => {
+    const destino = "/Orcamento";
+    const nomeObjeto = "Orçamento";
+
+    if(req.body.idOrcamento) {
+        Orcamento.findOne({_id: req.body.idOrcamento})
+            .then((orcamento) => {
+                orcamento.cliente = new mongoose.Types.ObjectId(req.body.dropCliente);
+                orcamento.descricao = req.body.txtDescricao;
+                orcamento.valor = Number(req.body.txtValor);
+                orcamento.modified = new Date();
+
+                orcamento.save()
+                    .then(() => {
+                        req.flash("Log", `${nomeObjeto} ${req.body.dropCliente} de ${req.body.txtValor} editado com sucesso`);
+                        res.redirect(destino);
+                    })
+                    .catch((err) => {
+                        req.flash("Erro", `Erro ao editar o ${nomeObjeto.toLowerCase()} ${req.body.dropCliente} de ${req.body.txtValor}. ${err}`);
+                        res.redirect(destino);
+                    });
+            })
+            .catch((err) => {
+                req.flash("Erro", `Erro ao editar o ${nomeObjeto.toLowerCase()} ${req.body.dropCliente} de ${req.body.txtValor}. ${err}`);
+                res.redirect(destino);
+            });
+    } else {
+        new Orcamento(
+            {
+                cliente: new mongoose.Types.ObjectId(req.body.dropCliente),
+                descricao: req.body.txtDescricao,
+                valor: Number(req.body.txtValor)
+            })
+            .save()
+            .then(() => {
+                req.flash("Log", `${nomeObjeto} ${req.body.dropCliente} de ${req.body.txtValor} incluído com sucesso`);
+                res.redirect(destino);
+            })
+            .catch((err) => {
+                req.flash("Erro", `Erro ao incluir o ${nomeObjeto.toLowerCase()} ${req.body.dropCliente} de ${req.body.txtValor}. ${err}`);
+                res.redirect(destino);
+            });
+    }
+});
+
+routerOrca.post("/del", async (req, res) => { 
+    const destino = "/Orcamento";
+    Orcamento.deleteOne({_id: req.body.idOrcamento})
+        .then(() => {
+            req.flash("Log", `Orçamento ${req.body.dropCliente} de R$ ${req.body.txtValor} removido`);
+            res.redirect(destino);
+        })
+        .catch((err) => {
+            req.flash("Erro", `Erro ao apagar o orçamento ${req.body.dropCliente} de R$ ${req.body.txtValor}. ${err}`);
+            res.redirect(destino);
+        });
 });
 
 module.exports = { routerOrca };
