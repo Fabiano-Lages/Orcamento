64fa07e6f3b87814c72f7a26a8f540186ead21ac
Fabiano Lages <fabianolages@gmail.com>
2024-07-10 19:29:11 -0300

    Atualização do projeto usando env para entrar no banco

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 const _Porta = 3200;
 const _DirName = require.main.path;
 
-const _Banco = "mongodb+srv://fabianolages:Nao0Conto@orcamento.xhdtuq5.mongodb.net/Orcamento?retryWrites=true&w=majority&appName=Orcamento";
+const _Banco = `mongodb+srv://${process.env.USUARIO_BANCO}:${process.env.PASS}@orcamento.xhdtuq5.mongodb.net/Orcamento?retryWrites=true&w=majority&appName=Orcamento`;
 
 module.exports = { _Porta, _DirName, _Banco };
